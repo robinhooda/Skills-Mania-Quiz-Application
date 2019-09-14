cf800582016a0d378a352d2df81ef429310ce817
Robin Hooda <robinhooda66@gmail.com>
2019-09-14 17:45:36 +0530

    changes made

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-const progressBarFull = document.getElementById("progressBarFull");
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
